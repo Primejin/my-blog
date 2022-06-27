3fe67d7ed4345942244a954b72b0222220fdcb1f
Primejin <sky585456@naver.com>
2022-06-28 00:02:05 +0900

    테스트 페이지 추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TestSpacePage() { //함수명은 AboutPage로 똑같이 해도 상관없어서, 전혀 중요하지 않지만 가능하면 알맞게 testPage로 바꿈
+    return (
+        <main>
+            <title>테스트 스페이스</title>
+            <h1>테스트 스페이스입니다.</h1>
+            
+        </main>
+    ); 
+}
+
+export default TestSpacePage; //바깥에서 얘한테 접근
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TestPage() { //함수명은 AboutPage로 똑같이 해도 상관없어서, 전혀 중요하지 않지만 가능하면 알맞게 testPage로 바꿈
+    return (
+        <main>
+            <title>테스트 페이지</title>
+            <h1>테스트 페이지입니다.</h1>
+            
+        </main>
+    ); 
+}
+
+export default TestPage; //바깥에서 얘한테 접근
