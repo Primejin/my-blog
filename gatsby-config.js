caae16dc180454960174b509d50776f18c7519fe
Primejin <sky585456@naver.com>
2022-06-28 16:30:10 +0900

    blog  렉토 mdx 파일들 생성, graphql로 읽기

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {  //블로그 안에 있는 파일들 읽어오기
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 };
