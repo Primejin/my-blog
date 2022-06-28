3544b85e938e9b5570e84374b0e21b58674abc6e
Primejin <sky585456@naver.com>
2022-06-28 16:41:02 +0900

    블로그 포스트 리스트 생성

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,29 @@
 import React from "react"
 import Layout from "../components/Layout";
+import { useStaticQuery, graphql } from "gatsby";
+
 
 function BlogPage() {
+    const data = useStaticQuery(graphql`
+    query {
+        allFile {
+            nodes {
+                name
+        }
+      }
+    }
+  `);
     return (
         <Layout pageTitle="Blog">
           <p>
             Hi there! I'm the proud creator of this site, which I built with Gatsby.
           </p>
+
+          <ul>
+            {data.allFile.nodes.map(node => (
+            <li key={node.name}>{node.name}</li>
+            ))}
+          </ul>
         </Layout>
     );
 }
