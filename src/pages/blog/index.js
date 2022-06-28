c4c460f6d231f2a54553c4e1f7aefb293060ba3f
Primejin <sky585456@naver.com>
2022-06-28 18:21:41 +0900

    글 리스트에서 포스트페이지로 링크까지 완성

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,35 +1,36 @@
 import React from "react"
 import Layout from "../../components/Layout";
-import { useStaticQuery, graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import { Link, graphql } from "gatsby";
 
 
-function BlogPage() {
-    const data = useStaticQuery(graphql`
-    query {
-        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-            nodes {
-              frontmatter {
-                date(formatString: "MMMM D, YYYY")
-                title
-              }
-              id
-              body
-            }
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        slug
+      }
     }
-  `);
-    return (
-        <Layout pageTitle="My Blog Posts">
-            {data.allMdx.nodes.map(node => (
-            <article key={node.id}>
-                <h2>{node.frontmatter.title}</h2>
-                <p>Posted: {node.frontmatter.date}</p>
-                <MDXRenderer>{node.body}</MDXRenderer> 
-            </article>
-            ))}
-        </Layout>
-    );
-}
+  }
+`;
 
 export default BlogPage; //바깥에서 얘한테 접근
