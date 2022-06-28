90831479e57264793f985335d5bb390d8354193a
Primejin <sky585456@naver.com>
2022-06-28 17:28:08 +0900

    블로그 내용 표시

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,33 @@
 import React from "react"
 import Layout from "../components/Layout";
 import { useStaticQuery, graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 
 function BlogPage() {
     const data = useStaticQuery(graphql`
     query {
-        allFile {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
             nodes {
-                name
+              frontmatter {
+                date(formatString: "MMMM D, YYYY")
+                title
+              }
+              id
+              body
+            }
         }
-      }
     }
   `);
     return (
-        <Layout pageTitle="Blog">
-          <p>
-            Hi there! I'm the proud creator of this site, which I built with Gatsby.
-          </p>
-
-          <ul>
-            {data.allFile.nodes.map(node => (
-            <li key={node.name}>{node.name}</li>
+        <Layout pageTitle="My Blog Posts">
+            {data.allMdx.nodes.map(node => (
+            <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <MDXRenderer>{node.body}</MDXRenderer> 
+            </article>
             ))}
-          </ul>
         </Layout>
     );
 }
