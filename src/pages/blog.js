def73799ab064e8490ce22479d4360bdebeb4d1d
Primejin <sky585456@naver.com>
2022-06-28 15:41:09 +0900

    사이트 이름 표시, 블로그 페이지 추가

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout";
+
+function BlogPage() {
+    return (
+        <Layout pageTitle="Blog">
+          <p>
+            Hi there! I'm the proud creator of this site, which I built with Gatsby.
+          </p>
+        </Layout>
+    );
+}
+
+export default BlogPage; //바깥에서 얘한테 접근
